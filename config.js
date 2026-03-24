640cee6828840074fa3b1ddb6968366c2ab199cd
jartmie-beep <jartmie@gmail.com>
2026-03-24 22:16:53 +0100

    config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     SELLER_USER_ID: '1485338157011701930',
     SHOP_NAME: 'Robux SHOP',
     EMBED_COLOR: 0x00FF00,
-    LOGO_URL: 'https://image2url.com/r2/default/images/1774377555031-1172f9e1-782c-432b-8f88-402324deb585.png',
+    LOGO_URL: 'https://customer-assets.emergentagent.com/job_legit-check-bot/artifacts/p88hv2qj_Gemini_Generated_Image_2yd1z22yd1z22yd1.png',
     TICKET_KEYWORD: 'ticket',
     LEGIT_CHECK_CHANNEL: '✅﹕legit-check'
 };
