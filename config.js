ac2b029a8db4b5b186d76289b87415e53550b501
jartmie-beep <jartmie@gmail.com>
2026-03-25 15:39:42 +0100

    config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     OWNER_ROLE_NAME: 'OWNER',
-    SELLER_USER_ID: '1485338157011701930',
+    SELLER_USER_ID: '1298681597473718357',
     SHOP_NAME: 'Robux SHOP',
     EMBED_COLOR: 0x00FF00,
-    LOGO_URL: 'https://customer-assets.emergentagent.com/job_legit-check-bot/artifacts/p88hv2qj_Gemini_Generated_Image_2yd1z22yd1z22yd1.png',
+    LOGO_URL: 'https://image2url.com/r2/default/images/1774449217451-e330583a-359f-41a2-b7e0-8300eab4e4b7.jpg',
     TICKET_KEYWORD: 'ticket',
     LEGIT_CHECK_CHANNEL: 'legit-check'
 };
